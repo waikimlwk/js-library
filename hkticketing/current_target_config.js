4aa2f9451d1fa21df906bfc8193ade04296a79bf
William Leung <waikimlwk@gmail.com>
2022-10-19 23:28:46 +0800

    Update current_target_config.js

--- hkticketing/current_target_config.js
+++ hkticketing/current_target_config.js
@@ -1,8 +1,8 @@
 let hkticketing_config = {};
 hkticketing_config['event_name'] = 'JUSTI1122';
-hkticketing_config['target_date_index'] = 1;
-hkticketing_config['target_area'] = 0;    // 0 = best seat, 1 = most expensive seat
-hkticketing_config['target_buy_ticket_number'] = 4;
+hkticketing_config['target_date_index'] = 2;
+hkticketing_config['target_area'] = 1;    // 0 = best seat, 1 = most expensive seat
+hkticketing_config['target_buy_ticket_number'] = 5;
 
 
 
