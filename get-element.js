b27799dc8850450ceb7a4e606f21df91c9a548e5
William Leung <waikimlwk@gmail.com>
2022-10-21 12:27:45 +0800

    Update get-element.js

--- get-element.js
+++ get-element.js
@@ -2,3 +2,9 @@ function get_element_by_xpath(xpath) {
   let result_element = document.evaluate (xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   return result_element;
 }
+
+
+function get_angluarjs_scope(ctrlName) {
+    var sel = 'div[ng-controller="' + ctrlName + '"]';
+    return angular.element(sel).scope();
+}
