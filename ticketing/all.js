da7cdc79cdf4c1b98202d115e9b9caabb8618eff
William Leung <waikimlwk@gmail.com>
2023-01-18 22:04:01 +0800

    Update all.js

--- ticketing/all.js
+++ ticketing/all.js
@@ -164,3 +164,59 @@ function randomIntFromInterval(min, max) { // min and max included
 function int_to_char(int_value) {
     return String.fromCharCode(int_value);
 }
+
+
+
+function eraseCookie (cookieName) {
+    //--- ONE-TIME INITS:
+    //--- Set possible domains. Omits some rare edge cases.?.
+    var domain      = document.domain;
+    var domain2     = document.domain.replace (/^www\./, "");
+    var domain3     = document.domain.replace (/^(\w+\.)+?(\w+\.\w+)$/, "$2");;
+
+    //--- Get possible paths for the current page:
+    var pathNodes   = location.pathname.split ("/").map ( function (pathWord) {
+        return '/' + pathWord;
+    } );
+    var cookPaths   = [""].concat (pathNodes.map ( function (pathNode) {
+        if (this.pathStr) {
+            this.pathStr += pathNode;
+        }
+        else {
+            this.pathStr = "; path=";
+            return (this.pathStr + pathNode);
+        }
+        return (this.pathStr);
+    } ) );
+
+    ( eraseCookie = function (cookieName) {
+        //--- For each path, attempt to delete the cookie.
+        cookPaths.forEach ( function (pathStr) {
+            //--- To delete a cookie, set its expiration date to a past value.
+            var diagStr     = cookieName + "=" + pathStr + "; expires=Thu, 01-Jan-1970 00:00:01 GMT;";
+            document.cookie = diagStr;
+
+            document.cookie = cookieName + "=" + pathStr + "; domain=" + domain  + "; expires=Thu, 01-Jan-1970 00:00:01 GMT;";
+            document.cookie = cookieName + "=" + pathStr + "; domain=" + domain2 + "; expires=Thu, 01-Jan-1970 00:00:01 GMT;";
+            document.cookie = cookieName + "=" + pathStr + "; domain=" + domain3 + "; expires=Thu, 01-Jan-1970 00:00:01 GMT;";
+        } );
+    } ) (cookieName);
+}
+
+function deleteAllCookie() {
+    var cookieList  = document.cookie.split (/;\s*/);
+    for (var J = cookieList.length - 1;   J >= 0;  --J) {
+        var cookieName = cookieList[J].replace (/\s*(\w+)=.+$/, "$1");
+        eraseCookie (cookieName);
+    }
+}
+
+function listCookies () {
+    var cookieList  = document.cookie.split (/;\s*/);
+
+    for (var J = 0, numCookies = cookieList.length;   J < numCookies;  ++J) {
+        console.log ("Cookie ", J, ": ", cookieList[J]);
+    }
+}
+
+
