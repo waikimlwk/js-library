8330ec32b9e87871f15033ba4efc8e1f1313a10a
William Leung <waikimlwk@gmail.com>
2023-02-07 10:33:27 +0800

    Update all.js

--- ticketing/all.js
+++ ticketing/all.js
@@ -64,7 +64,7 @@ function get_angluarjs_scope(ctrlName) {
         });
     }
 
-    function waitForXpathNode(xpath) {
+    async function waitForXpathNode(xpath) {
         while (true) {
             let element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
             if (element) {
